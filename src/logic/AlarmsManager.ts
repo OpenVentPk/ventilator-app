10d2664575959d71b7936778c6a3f714a1758b77
themakshter <mohammad_ali93@live.com>
2020-05-14 01:58:25 +0100

    Add looping sound to alarms

--- src/logic/AlarmsManager.ts
+++ src/logic/AlarmsManager.ts
@@ -1,7 +1,18 @@
 import { showMessage, hideMessage } from 'react-native-flash-message';
+import Sound from 'react-native-sound';
+
+Sound.setCategory('Playback');
 
 function AlarmsManager() {
   let currentAlarms: Array<string> = [];
+  let alarmSound = new Sound('alarm.mp3', Sound.MAIN_BUNDLE, (error: any) => {
+    if (error) {
+      console.log('failed to load the sound', error);
+      return;
+    }
+    // loaded successfully
+    console.log('duration in seconds: ' + alarmSound.getDuration() + ' number of channels: ' + alarmSound.getNumberOfChannels());
+  });
 
   function onNewReading(reading: any) {
     const newAlarms: any = reading.alarms;
@@ -17,6 +28,7 @@ function AlarmsManager() {
   function handleCurrentAlarms(alarms: Array<string>): void {
     if (alarms.length === 0) {
       hideMessage();
+      alarmSound.stop();
     } else {
       displayAlarmsBanner();
     }
@@ -24,11 +36,25 @@ function AlarmsManager() {
 
   function displayAlarmsBanner(): void {
     const alarmsText = currentAlarms.join('\n');
+    alarmSound.play((success) => {
+      console.log('hello');
+      if (success) {
+        console.log('successfully finished playing');
+      } else {
+        console.log('playback failed due to audio decoding errors');
+      }
+    });
+  alarmSound.setNumberOfLoops(-1);
+
     showMessage({
       message: 'Alarm(s) active',
       description: alarmsText,
       type: 'danger',
       autoHide: false,
+      hideOnPress: true,
+      onPress: () => {
+        alarmSound.stop();
+      },
     });
   }
 
