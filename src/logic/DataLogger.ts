ea6bd9825f66fef8a2f8ba74cc90dd8d613505e2
themakshter <mohammad_ali93@live.com>
2020-06-05 01:49:08 +0100

    fix writing to file

--- src/logic/DataLogger.ts
+++ src/logic/DataLogger.ts
@@ -1,19 +1,25 @@
 import * as RNFS from 'react-native-fs';
 import Alarms from '../constants/Alarms';
+import SetParameter from '../interfaces/SetParameter';
 
 export default function dataLogger() {
-  const nowTimeStamp: string = new Date().toISOString().replace(':','-');
-  const logFile: string = `${RNFS.DocumentDirectoryPath}/${nowTimeStamp}_readings.csv`;
+  const nowTimeStamp: string = new Date().toISOString().replace(/\.|:/g, '-');
+  const logDirectory: string = `${RNFS.ExternalDirectoryPath}/readings`;
+  const logFile: string = `${nowTimeStamp}-readings.csv`;
+
+  const folderCreationPromise = RNFS.mkdir(logDirectory);
 
   function onDataReading(reading: any) {
     var readingInCsv = getCsvFormat(reading);
-    RNFS.write(logFile, readingInCsv)
-      .then(() => {
-        console.log(`written to ${logFile}`);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
+    folderCreationPromise.then(() => {
+      RNFS.write(`${logDirectory}/${logFile}`, readingInCsv)
+        .then(() => {
+          console.log(`written to ${logFile}`);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    });
   }
 
   function getCsvFormat(reading: any): string {
@@ -56,6 +62,10 @@ export default function dataLogger() {
       getAlarmsInCsvFormat(alarms),
     ].join(',');
     return readingsString;
+  }
+
+  function getSetParameterCsvFormat(parameter: SetParameter): string {
+
   }
 
   function getAlarmsInCsvFormat(alarms: string[]): string {
