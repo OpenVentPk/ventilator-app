4319c8a103ed28753a0f0c5f8481aa8ef91564c7
themakshter <mohammad_ali93@live.com>
2020-07-18 22:01:33 +0100

    add remaining files

--- src/logic/AppLogger.ts
+++ src/logic/AppLogger.ts
@@ -3,20 +3,19 @@ import { logger, configLoggerType } from 'react-native-logs';
 import { rnFsFileAsync } from 'react-native-logs/dist/transports/rnFsFileAsync';
 import { ansiColorConsoleSync } from 'react-native-logs/dist/transports/ansiColorConsoleSync';
 import * as RNFS from 'react-native-fs';
-import { getDirectoryPath, getTimestampedFileName } from '../utils/FileUtils';
+import { getTimestampedFileName } from '../utils/FileUtils';
 
 console.log('starting global logger');
-const directorySubfolder: string = 'app-logs';
-const logDirectory: string = getDirectoryPath(directorySubfolder);
-const logFile: string = getTimestampedFileName();
+
+const logDirectory: string = `${RNFS.ExternalDirectoryPath}/app-logs`;
 RNFS.mkdir(logDirectory);
 
 const config: configLoggerType = {
   transport: (msg, level, options) => {
     ansiColorConsoleSync(msg, level, options);
     rnFsFileAsync(msg, level, {
-      loggerName: logFile,
-      loggerPath: logDirectory,
+      loggerName: getTimestampedFileName(),
+      loggerPath: `${logDirectory}`,
     });
   },
 };
